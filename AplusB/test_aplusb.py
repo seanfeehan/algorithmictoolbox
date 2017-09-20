78d13ced504b0b03fd11a5ff1da46f86abf66d61
sfeehan <sfeehan@drivewyze.com>
2017-09-20 13:25:40 -0600

    Got Unit Testing Working

--- AplusB/test_aplusb.py
+++ AplusB/test_aplusb.py
@@ -4,9 +4,15 @@ from AplusB import AplusB
 
 class TestAplusB(TestCase):
 
-    def test_add1(self):
-        retval = AplusB.add_numbers(1, 2)
-        self.assertEquals(retval, 3)
 
-    def test_add_numbers(self):
-        self.fail()
+    def test_add_numbers1(self):
+        retval = AplusB.add_numbers(self, 1, 2)
+        self.assertEqual(retval, 3)
+
+    def test_add_numbers2(self):
+        retval = AplusB.add_numbers(self, 44, 55)
+        self.assertEqual(retval, 99)
+
+    def test_connor_mcdavid(self):
+        retval = AplusB.add_numbers(self, 44, 53)
+        self.assertEqual(retval, 97)
